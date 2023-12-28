c986f48803f13bfac18f3287a086c37eb62b9fb6
ManojSynectiks <manoj.sharma@synectiks.com>
2023-12-28 12:23:52 +0530

    update script issue fix

--- src/main/resources/dbscript/V2__update_default_data.sql
+++ src/main/resources/dbscript/V2__update_default_data.sql
@@ -1,9 +1,6 @@
 
-update policy_assigned_permissions set permission_category_name = (select pc.name from permission_category pc
-where policy_assigned_permissions.permission_category_id = pc.id)
+update policy_assigned_permissions set permission_category_name = (select pc.name from permission_category pc where policy_assigned_permissions.permission_category_id = pc.id);
 
-update policy_assigned_permissions set permission_name  = (select p.name from "permission"  p
-where policy_assigned_permissions.permission_id  = p.id)
+update policy_assigned_permissions set permission_name  = (select p.name from "permission"  p where policy_assigned_permissions.permission_id  = p.id);
 
-update policy_assigned_permissions set policy_name = (select p.name from "policy"  p
-where policy_assigned_permissions.policy_id  = p.id)
+update policy_assigned_permissions set policy_name = (select p.name from "policy"  p where policy_assigned_permissions.policy_id  = p.id);
